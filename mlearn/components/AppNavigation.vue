7e75d38ddcdda6c3e563969e63cdd72c084cb602
Marco Silva <marco.antonio.almeida.silva@gmail.com>
2023-02-17 22:30:56 -0300

    improved navigation bar

--- mlearn/components/AppNavigation.vue
+++ mlearn/components/AppNavigation.vue
@@ -1,17 +1,35 @@
 <script setup lang="ts">
 const { cover } = useAppConfig()
 
-const navigation = [
+import { ref } from 'vue'
+
+const navigation = ref([
   {
     "title":"Home",
-    "_path":"/"
+    "_path":"/",
+    "icon":"emojione:house",
+    "show_title": true
   },
   {
     "title":"collab",
-    "_path":"/collab"
+    "_path":"/collab",
+    "icon":"logos:openai-icon",
+    "show_title": true
+  },
+  {
+    "title": "github",
+    "_path": "https://github.com/MarcoAAlmeida",
+    "icon": "logos:github-icon"
+  },
+  {
+    "title": "linkedIn",
+    "_path": "https://www.linkedin.com/in/marcoaasilva/",
+    "icon": "logos:linkedin-icon"
   }
+])
 
-]
+const target = (attribute: string) => attribute.startsWith("http") ? "_blank" : "_self"
+
 </script>
 
 <template>
@@ -20,10 +38,11 @@ const navigation = [
       v-for="link of navigation"
       :key="link._path"
       :to="link._path"
+      :target="target(link._path)"
       active-class="font-bold"
       class="mr-2"
     >
-      {{ link.title }}
+      <Icon :name="link.icon" /> <span v-if="link.show_title">{{ link.title }}</span>
     </NuxtLink>
   </div>
 </template>
