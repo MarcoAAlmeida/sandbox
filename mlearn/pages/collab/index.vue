1b58d37c84b338755dabdf032120c2ee55f679cb
Marco Silva <marco.antonio.almeida.silva@gmail.com>
2023-03-21 22:44:22 -0300

    changed layout of collab page

--- mlearn/pages/collab/index.vue
+++ mlearn/pages/collab/index.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 const defaultOptions = [
   'img/ai_collab/dalle-1.png',
   'img/ai_collab/dalle-2.png',
@@ -9,24 +9,32 @@ const defaultOptions = [
   'img/ai_collab/dalle-7.png'
 ]
 
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+const query: QueryBuilderParams = { path: '/collab', where: { published: { $eq: true } }, limit: 15, sort: [{ authorship: -1 }] }
+
+const { data: publishedQuery } = await useAsyncData('published', () => {
+  return queryContent('/collab').where({ published: true }).sort({ authorship: -1 }).find()
+})
+
+
 </script>
+
 <template>
   <AppLayout>
-    <ContentList v-slot="{ list }" path="/collab">
-      <div class="flex flex-wrap">
-        <div v-for="article in list" :key="article._path" class="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 m-1 min-w-[20rem]">
+    <ContentList v-slot="{ list }" :query="query">
+      <div class="flex justify-center flex-wrap">
+        <div v-for="article in list" :key="article._path" class="w-full max-w-md px-8 py-4 mr-5 ml-5 mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800">
           <a class="text-md" :href="article._path" :title="article.description">
-            <div class="px-4 py-2">
-              <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">{{ article.title }}</h1>
-              <p class="mt-1 text-sm text-gray-600 dark:text-gray-400 min-h-[3rem]">
-                {{ article.description }}
-              </p>
+            <div class="flex justify-center -mt-16 md:justify-end">
+              <img class="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400" alt="Testimonial avatar" :src="article.cover">
             </div>
 
-            <img class="object-cover w-full h-48 mt-2" :src="useCover(article.cover, defaultOptions)" alt="{{ article.title }}">
+            <h2 class="mt-2 text-xl font-semibold text-gray-800 dark:text-white md:mt-0">{{ article.title }}</h2>
 
-            <div class="flex items-center justify-between px-4 py-2 text-white dark:text-black">
-              <p>{{ article.tags }}</p>
+            <p class="mt-2 text-sm text-gray-600 dark:text-gray-200">{{ article.description }}</p>
+
+            <div class="flex justify-end mt-4">
+              <span class="text-sm font-light text-gray-600 dark:text-gray-400">{{ article.authorship }}</span>
             </div>
           </a>
         </div>
