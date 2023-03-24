f051de1c66a448c9bc60d53b3296413e8f7a9d84
Marco Silva <marco.antonio.almeida.silva@gmail.com>
2023-03-24 17:27:59 -0300

    added Twitter Card SEO

--- mlearn/layouts/default.vue
+++ mlearn/layouts/default.vue
@@ -16,5 +16,17 @@
 </style>
 <script setup lang="ts">
 const { page } = useContent()
-useContentHead(page)
+
+const content = await page.value;
+
+useHead({
+  meta: [
+    { name: 'image', property:'og:image', content: content.cover },
+    { name: 'twitterImage', property:'twitter:image', content: 'https://main--funny-crisp-0cbebe.netlify.app/'+content.cover },
+    { name: 'twitterCard', property:'twitter:card', content: "summary_large_image" },
+    { name: 'author', content: 'marcoalmeida.dev.br@gmail.com' },
+    { name: 'title', property:'og:title', content: content.title },
+    { name: 'description', property:'og:description', content: content.description }
+  ],
+})
 </script>
