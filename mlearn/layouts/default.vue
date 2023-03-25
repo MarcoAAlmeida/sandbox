ca2931bab1240d38d0ef292af317d6a4d220464d
Marco Silva <marco.antonio.almeida.silva@gmail.com>
2023-03-25 19:34:25 -0300

    added Sparks article

--- mlearn/layouts/default.vue
+++ mlearn/layouts/default.vue
@@ -22,7 +22,7 @@ const content = await page.value;
 useHead({
   meta: [
     { name: 'image', property:'og:image', content: content.cover },
-    { name: 'twitterImage', property:'twitter:image', content: 'https://main--funny-crisp-0cbebe.netlify.app/'+content.cover },
+    { name: 'twitterImage', property:'twitter:image', content: content.cover },
     { name: 'twitterCard', property:'twitter:card', content: "summary_large_image" },
     { name: 'author', content: 'marcoalmeida.dev.br@gmail.com' },
     { name: 'title', property:'og:title', content: content.title },
