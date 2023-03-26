0988349ab66fa0ae6266f1aa08853046d1d35aff
Marco Silva <marco.antonio.almeida.silva@gmail.com>
2023-03-26 17:16:49 -0300

    added computerphile article

--- mlearn/layouts/default.vue
+++ mlearn/layouts/default.vue
@@ -4,16 +4,6 @@
   </AppLayout>
 </template>
 
-<style lang="postcss">
-/* Customize headers to remove default underline */
-.prose h2 a,
-.prose h3 a {
-  @apply no-underline;
-  &:hover {
-    @apply border-b border-neutral-600 dark:border-neutral-300;
-  }
-}
-</style>
 <script setup lang="ts">
 const { page } = useContent()
 
