4610b2ae8a36edd0ddba50176a0786ca22270fe8
Marco Silva <marco.antonio.almeida.silva@gmail.com>
2023-04-16 18:37:34 -0300

    change should trigger a Github action

--- mlearn/components/Hero.vue
+++ mlearn/components/Hero.vue
@@ -7,8 +7,13 @@
           <div class="lg:max-w-lg">
             <h1 class="text-2xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Welcome to my<br/> <span class="text-blue-500">Machine Learning blog</span></h1>
 
-            <p class="mt-3 text-gray-600 dark:text-gray-400">this is still under construction</p>
-            <p class="mt-3 text-gray-600 dark:text-gray-400">In the meantime, please connect with <span class="font-medium text-blue-500">MarcoAASilva</span> on</p>
+            <p class="mt-3 text-gray-600 dark:text-gray-400">In the meantime, please drop <a href="https://www.linkedin.com/in/marcoaasilva/" target="_blank"><span class="font-medium text-blue-500">me a message on LinkedIn</span></a></p>
+
+            <div class="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
+              <a href="https://www.linkedin.com/in/marcoaasilva/" target="_blank">
+                <Icon name="logos:linkedin-icon" size="4em" />
+              </a>
+            </div>
 
             <div class="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
               <a href="https://www.linkedin.com/in/marcoaasilva/" target="_blank">
